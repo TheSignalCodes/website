d947b42ae4c078f18792a503ffc08b9ca0dff49d
Serchinastico <github@serchinastico.com>
2024-03-25 13:48:19 +0100

    Refactor styles into multiple files

---
+++ src/styles/index.ts
@@ -0,0 +1,10 @@
+import "./tailwind.css";
+import "./fonts.css";
+import "./base.css";
+
+import "./companies.css";
+import "./decorations.css";
+import "./header.css";
+import "./how-we-work.css";
+import "./services.css";
+import "./us.css";
