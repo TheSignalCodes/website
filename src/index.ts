7a2c6e087695224678a31cff814d3150372caa2b
Serchinastico <github@serchinastico.com>
2024-03-25 19:46:21 +0100

    Implement some basic menu

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 import "./styles";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openMenu = document.getElementById("open-menu");
+  const closeMenu = document.getElementById("close-menu");
+  const navigation = document.getElementById("navigation");
+  const navLinks = document.querySelectorAll("nav a");
+
+  openMenu.addEventListener("click", () => {
+    navigation.style.right = "0";
+  });
+
+  closeMenu.addEventListener("click", () => {
+    navigation.style.right = "-100vw";
+  });
+
+  navLinks.forEach((navLink) =>
+    navLink.addEventListener("click", () => {
+      navigation.style.right = "-100vw";
+    })
+  );
+});
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,9 +2,10 @@ import "./tailwind.css";
 import "./fonts.css";
 import "./base.css";
 
-import "./companies.css";
 import "./decorations.css";
 import "./header.css";
 import "./how-we-work.css";
+import "./navigation.css";
 import "./services.css";
 import "./us.css";
+import "./why-us.css";
